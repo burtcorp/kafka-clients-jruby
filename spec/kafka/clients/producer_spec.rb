74d43a0452e86163c3667f7b6a4a3307f54351b1
Theo <theo@iconara.net>
2016-09-13 16:51:08 +0200

    Rewrite the #send specs to be less verbose
    I really like aggregate_failures.

--- spec/kafka/clients/producer_spec.rb
+++ spec/kafka/clients/producer_spec.rb
@@ -46,37 +46,16 @@ module Kafka
           future.get(timeout: 5)
         end
 
-        context 'returns a future that resolves to a record metadata object that' do
-          let :record_metadata do
-            producer.send('topictopic', 'hello', 'world').get(timeout: 5)
-          end
-
-          it 'knows the record\'s offset' do
-            expect(record_metadata.offset).to be_a(Fixnum)
-          end
-
-          it 'knows the record\'s partition' do
-            expect(record_metadata.partition).to be_a(Fixnum)
-          end
-
-          it 'knows the record\'s timestamp' do
-            expect(record_metadata.timestamp).to be_within(5).of(Time.now)
-          end
-
-          it 'knows the record\'s topic' do
-            expect(record_metadata.topic).to eq('topictopic')
-          end
-
-          it 'knows the record\'s checksum' do
-            expect(record_metadata.checksum).to be_a(Fixnum)
-          end
-
-          it 'knows the record\'s serialized key size' do
-            expect(record_metadata.serialized_key_size).to be_a(Fixnum)
-          end
-
-          it 'knows the record\'s serialize value size' do
-            expect(record_metadata.serialized_value_size).to be_a(Fixnum)
+        it 'returns a future that resolves to a record metadata object describing the saved record' do
+          metadata = producer.send('topictopic', 'hello', 'world').get(timeout: 5)
+          aggregate_failures do
+            expect(metadata.offset).to be_a(Fixnum)
+            expect(metadata.partition).to be_a(Fixnum)
+            expect(metadata.timestamp).to be_within(5).of(Time.now)
+            expect(metadata.topic).to eq('topictopic')
+            expect(metadata.checksum).to be_a(Fixnum)
+            expect(metadata.serialized_key_size).to be_a(Fixnum)
+            expect(metadata.serialized_value_size).to be_a(Fixnum)
           end
         end
       end
